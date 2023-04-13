0904a00674161b1186fef912881bf4013409ceb6
Agtheo09 <agtheo09@gmail.com>
2023-04-13 16:37:24 +0300

    2D Interpolation

--- src/PathEvaluator.py
+++ src/PathEvaluator.py
@@ -2,14 +2,17 @@ import math
 import numpy as np
 import numpy as np
 from scipy.stats import linregress
+from scipy.interpolate import interp1d
 
 class PathEvaluator:
     pathPoints = np.array([]) # [[x1, y1], [x2, y2], [x3, y3], ... [xn, yn]]
+    timestamps = np.array([])
     linearity_index = -1
     startStopOffset = []
     
-    def __init__(self, pathPoints):
+    def __init__(self, pathPoints, timestamps):
         self.pathPoints = pathPoints
+        self.timestamps = timestamps
     
     def pathLinearity(self):        
         allX = self.pathPoints[:, 0]
@@ -20,7 +23,18 @@ class PathEvaluator:
         self.linearity_index = r_value**2
         
     def pathInterpolation(self):
-        pass
+        tempX = self.pathPoints[:, 0]
+        tempY = self.pathPoints[:, 1]
+        
+        t_new = np.linspace(min(self.timestamps), max(self.timestamps), 100)
+
+        f1 = interp1d(self.timestamps, tempX, kind='cubic')
+        f2 = interp1d(self.timestamps, tempY, kind='cubic')
+
+        x_new = f1(t_new)
+        y_new = f2(t_new)
+        
+        return np.column_stack(x_new, y_new)
     
     
     def startStopError(self):
