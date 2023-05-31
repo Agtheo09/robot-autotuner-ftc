80529e5d0112f78ba5e5faef867894d647e481d2
Agtheo09 <agtheo09@gmail.com>
2023-05-31 15:30:03 +0300

    Removing Unnecessary Code and Files

--- main.py
+++ main.py
@@ -1,29 +1,22 @@
 import cv2 as cv
 import numpy as np
-from cvzone import FPS
+import time
 
 # Custom Classes
 from src.Apriltagging import AprilTagging
 from src.Localizer import Localizer
 from src.PathCapturer import PathCapturer
 from src.Datalogger import Datalogger
-from src.PathEvaluator import PathEvaluator
 from src.CameraCalibrator import CameraCalibrator
-from src.WebcamStreaming import WebcamStreaming
+from src.VideoStream import VideoStream
 
 if __name__ == "__main__":
     # Capturing Input
-    # cap = WebcamStreaming(src="./imgs/sample-vid.mp4")
+    # cap = cv.VideoCapture("./imgs/sample-vid.mp4")
+    cap = VideoStream("./imgs/sample-vid.mp4").start()
     # cap.start()
-    cap = cv.VideoCapture("./imgs/sample-vid.mp4")
 
-    fpsReader = FPS()
-    minFPS = 200
-    maxFPS = 0
-    sumFps = 0
-    counter = 1
-
-    # Initializing Custom Classes
+    # # Initializing Custom Classes
     tagDetector = AprilTagging()
     localizer = Localizer(numOfRobotTags=2, tagOffset=np.array([0, 0]))
     datalogger = Datalogger()
@@ -31,19 +24,17 @@ if __name__ == "__main__":
 
     pathCapturerRunning = False
 
-    frame = cap.read()[1]
+    frame = cap.read()
 
     tagDetector.update(frame)
+    fieldTagsCenters = tagDetector.getTagCentersByIds(localizer.fieldTagIds)
     cam_calibr = CameraCalibrator(
-        tagDetector.getTagCenterById(localizer.fieldTagIds[0]),
-        tagDetector.getTagCenterById(localizer.fieldTagIds[1]),
-        tagDetector.getTagCenterById(localizer.fieldTagIds[2]),
-        tagDetector.getTagCenterById(localizer.fieldTagIds[3]),
+        fieldTagsCenters,
         frame_size=(frame.shape[0], frame.shape[0]),
         padding=50,
     )
 
-    reverted = cam_calibr.applyPerspectiveRevert(frame)
+    reverted = cam_calibr.applyCalibrations(frame)
 
     localizer.updateFieldTagPositions(cam_calibr.getFieldTagPositions())
 
@@ -51,76 +42,57 @@ if __name__ == "__main__":
 
     robotTagPositions = [(0, 0), (0, 0)]
 
-    # while cap.isQEmpty():
+    counter = 1
+    sumOfFPS = 0
+    # while cap.more():
     while True:
-        frame = cap.read()[1]
+        start_time = time.time()
+        frame = cap.read()
         if frame is None:
             continue
 
         # Calibrating Camera
-        calibrated = cam_calibr.applyPerspectiveRevert(frame)
+        calibrated = cam_calibr.applyCalibrations(frame)
 
         # Detecting Apriltags
         detectedMat = tagDetector.update(calibrated)
 
         # Update Tag Positions
-        robotTagPositions = list(
-            map(
-                lambda x: tagDetector.getTagCenterById(x), localizer.robotTagIds
-            )
-        )
-
-        # Calulate Poses, Velocities etc
-        localizer.update(robotTagPositions)
-
-        robotPose = localizer.getRobotPose()
-        robotSpeed = localizer.getRobotCurrentVelocity()
-
-        # print(localizer.positioningInstructions())
-        print("Pose: ", np.asarray(robotPose))
-
-        capturer.update(robotPose)
-
-        if cv.waitKey(1) == ord("c"):
-            # if not capturer.captureEnabled and localizer.atTheCorrectSpot():
-            if not capturer.captureEnabled:
-                capturer.startCapturing()
-        if cv.waitKey(1) == ord("v"):
-            if capturer.captureEnabled:
-                capturer.stopCapturing()
-
-        rawSize = frame.shape[:2]
-
-        # FPS
-        fps, plusFPS = fpsReader.update(
-            detectedMat, pos=(50, 80), color=(0, 255, 0), scale=5, thickness=5
-        )
-
-        # FPS Calculations
-        minFPS = fps if fps < minFPS else minFPS
-        maxFPS = fps if fps > maxFPS else maxFPS
-        sumFps += fps
-        counter += 1
-
-        # stacked_frames = np.hstack(
-        #     (cv.resize(frame, (720, 720)), cv.resize(plusFPS, (720, 720)))
+        # robotTagPositions = list(
+        #     tagDetector.getTagCentersByIds(localizer.robotTagIds)
         # )
 
-        # Display the stacked frames
-        # cv.imshow("Preview", stacked_frames)
+        # # Calulate Poses, Velocities etc
+        # localizer.update(robotTagPositions)
+
+        # robotPose = localizer.getRobotPose()
+        # robotVelocity = localizer.getRobotCurrentVelocity()
+
+        # # print(localizer.positioningInstructions())
+        # print("Pose: ", np.asarray(robotPose))
+
+        # capturer.update(robotPose)
+
+        # if cv.waitKey(1) == ord("c"):
+        #     # if not capturer.captureEnabled and localizer.atTheCorrectSpot():
+        #     if not capturer.captureEnabled:
+        #         capturer.startCapturing()
+        # if cv.waitKey(1) == ord("v"):
+        #     if capturer.captureEnabled:
+        #         capturer.stopCapturing()
+
         cv.imshow("Raw", cv.resize(frame, (720, 720)))
-        cv.imshow("Proccessed", cv.resize(plusFPS, (700, 700)))
-        # cv.imshow("Proccessed", plusFPS)
+        # cv.imshow("Proccessed", cv.resize(detectedMat, (700, 700)))
+        dt = time.time() - start_time + 0.000000000000000001
+        fps = 1 / dt
+        sumOfFPS += fps
+        counter += 1
+        print("Current FPS: ", fps)
 
         if cv.waitKey(5) & 0xFF == 27:
             break
 
-    # cap.stop()
-    # cap.join()
-    cap.release()
+    cap.stop()
+    # cap.release()
     cv.destroyAllWindows()
-    # FPS Calculations Preview
-    print("Max FPS: ", maxFPS)
-    print("Min FPS: ", minFPS)
-    print("Average FPS: ", sumFps / counter)
-    print("Total Frames Previewed: ", counter)
+    print("Average FPS: ", sumOfFPS / counter)
